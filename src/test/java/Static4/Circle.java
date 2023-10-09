4beffee517d65b732b7c2c7f9e5d87401f7f56ff
galstyanaida <aidagal1996@gmail.com>
2023-10-09 22:18:08 +0400

    5

---
+++ src/test/java/Static4/Circle.java
@@ -0,0 +1,7 @@
+package Static4;
+
+public class Circle {
+    public static double calculateArea(double radius){
+        return Math.PI * radius * radius;
+    }
+}
---
+++ src/test/java/Static4/Start3.java
@@ -0,0 +1,9 @@
+package Static4;
+
+public class Start3 {
+    public static void main(String [] args){
+        double radius= 4;
+        double area = Circle.calculateArea(radius);
+        System.out.println("The area is: "+ area);
+    }
+}
