037f128ff021547da2b6ff9ffdb3993007fcf5c5
galstyanaida <aidagal1996@gmail.com>
2023-10-09 22:09:08 +0400

    3

---
+++ src/test/java/Static3/MathOperations.java
@@ -0,0 +1,11 @@
+package Static3;
+
+public class MathOperations {
+    public static int square(int number){
+        return number*number;
+    }
+
+    public static int cube(int number2){
+        return number2 * number2 * number2;
+    }
+}
---
+++ src/test/java/Static3/Start.java
@@ -0,0 +1,8 @@
+package Static3;
+
+public class Start {
+    public static void main(String [] args){
+        System.out.println(MathOperations.cube(6));
+        System.out.println(MathOperations.square(8));
+    }
+}
